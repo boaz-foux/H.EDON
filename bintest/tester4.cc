e43d40a8e3b695d0bffc1730faa08410b47ddfe2
Boaz Foux <boazfoux@Boazs-MacBook-Pro-2.local>
2019-02-11 15:33:59 +0200

    final 0.7 (still beta) 	* adding object generator and a macro based bind for setter getter & validate 	* update licence 	* remove unneeded params and unused dependencies

--- bintest/tester4.cc
+++ bintest/tester4.cc
@@ -4,14 +4,12 @@
 #include "../src/hedon.h"
 
 int staticArray[8] = { 3,4,6,7,8,99,10,676};
+
 static const int i = 5558;
+
 void Init( v8::Handle<v8::Object> exports) {
-	// HEDON::Object( ("hello",1) ); 
-
 	HEDON::EXPORT(exports,staticArray);
-
 	HEDON::EXPORT(exports,i);
-
 }
 
 
